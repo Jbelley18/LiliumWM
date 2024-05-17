1963bf8c766322ef8631810e20922e9fce895d67
jbelley18 <Jbelley@wgu.edu>
2024-05-17 06:36:44 -0400

    Work in progress from last commit, might need jesus to take a look at this

--- src/main.c
+++ src/main.c
@@ -4,10 +4,8 @@
 #include <stdio.h>
 #include <math.h>
 
-// Global variables
 const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-// Define layout types
 typedef enum {
     VERTICAL_TILING,
     HORIZONTAL_TILING,
@@ -16,16 +14,14 @@ typedef enum {
 
 LayoutType currentLayout = VERTICAL_TILING;  // Start with vertical tiling
 
-// Monitor information
 typedef struct {
-    HMONITOR hMonitor;
+    MONITORINFOEX Monitor;
     RECT rcMonitor;
-} MONITORINFOEX;
+} MYMONITORINFOEX;
 
-MONITORINFOEX monitors[10];
+MYMONITORINFOEX monitors[10];
 int monitorCount = 0;
 
-// Forward declarations
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam);
 BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData);
@@ -34,156 +30,43 @@ void ApplyVerticalTiling(HWND* windows, int count);
 void ApplyHorizontalTiling(HWND* windows, int count);
 void ApplyGridLayout(HWND* windows, int count);
 
-// Main function
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASS wc = { 0 };
+
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
 
-    if (!RegisterClass(&wc)) {
-        return 1;  // Fail if class registration fails
-    }
+    RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(
-        0,                          // Optional styles
-        CLASS_NAME,                 // Window class name
-        L"Learn to Program Windows",// Window title
-        WS_OVERLAPPEDWINDOW,        // Window style
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,  // Size and position defaults
-        NULL,                       // Parent window    
-        NULL,                       // Menu
-        hInstance,                  // Instance handle
-        NULL                        // Additional application data
+        0,
+        CLASS_NAME,
+        L"Learn to Program Windows",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
     );
 
     if (hwnd == NULL) {
-        return 0;  // Exit if window creation fails
+        return 0;
     }
 
     ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
 
-    // Enumerate all monitors
-    monitorCount = 0;
+    // Enumerate monitors
     EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
 
-    // Apply the initial layout
-    ApplyLayout(currentLayout);
-
-    // Register hotkeys
-    RegisterHotKey(NULL, 1, MOD_ALT, 'V');  // Alt + V for Vertical Tiling
-    RegisterHotKey(NULL, 2, MOD_ALT, 'H');  // Alt + H for Horizontal Tiling
-    RegisterHotKey(NULL, 3, MOD_ALT, 'G');  // Alt + G for Grid Layout
-
-    // Standard message loop
-    MSG msg;
+    MSG msg = { 0 };
     while (GetMessage(&msg, NULL, 0, 0)) {
-        if (msg.message == WM_HOTKEY) {
-            switch (msg.wParam) {
-                case 1:  // Vertical Tiling
-                    currentLayout = VERTICAL_TILING;
-                    ApplyLayout(currentLayout);
-                    break;
-                case 2:  // Horizontal Tiling
-                    currentLayout = HORIZONTAL_TILING;
-                    ApplyLayout(currentLayout);
-                    break;
-                case 3:  // Grid Layout
-                    currentLayout = GRID_LAYOUT;
-                    ApplyLayout(currentLayout);
-                    break;
-            }
-        } else {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-    return (int)msg.wParam;
-}
-
-BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) {
-    monitors[monitorCount].hMonitor = hMonitor;
-    monitors[monitorCount].rcMonitor = *lprcMonitor;
-    monitorCount++;
-    return TRUE;
-}
-
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
-    static HWND windows[256];
-    static int count = 0;
-
-    if (IsWindowVisible(hwnd)) {
-        windows[count++] = hwnd;
-    }
-
-    if (count == 256) {
-        return FALSE;  // Avoid overflow
-    }
-
-    if (count > 0 && hwnd == NULL) {  // If we reached the end
-        LayoutType layout = (LayoutType)lParam;
-        if (layout == VERTICAL_TILING) {
-            ApplyVerticalTiling(windows, count);
-        } else if (layout == HORIZONTAL_TILING) {
-            ApplyHorizontalTiling(windows, count);
-        } else if (layout == GRID_LAYOUT) {
-            ApplyGridLayout(windows, count);
-        }
-        count = 0;  // Reset count for next enumeration
-    }
-
-    return TRUE;
-}
-
-void ApplyLayout(LayoutType layout) {
-    EnumWindows(EnumWindowsProc, (LPARAM)layout);
-}
-
-void ApplyVerticalTiling(HWND* windows, int count) {
-    for (int m = 0; m < monitorCount; m++) {
-        int monitorHeight = monitors[m].rcMonitor.bottom - monitors[m].rcMonitor.top;
-        int monitorWidth = monitors[m].rcMonitor.right - monitors[m].rcMonitor.left;
-        int windowHeight = monitorHeight / count;
-
-        for (int i = 0; i < count; i++) {
-            SetWindowPos(windows[i], HWND_TOP, monitors[m].rcMonitor.left, monitors[m].rcMonitor.top + i * windowHeight,
-                monitorWidth, windowHeight, SWP_NOZORDER);
-        }
-    }
-}
-
-void ApplyHorizontalTiling(HWND* windows, int count) {
-    for (int m = 0; m < monitorCount; m++) {
-        int monitorHeight = monitors[m].rcMonitor.bottom - monitors[m].rcMonitor.top;
-        int monitorWidth = monitors[m].rcMonitor.right - monitors[m].rcMonitor.left;
-        int windowWidth = monitorWidth / count;
-
-        for (int i = 0; i < count; i++) {
-            SetWindowPos(windows[i], HWND_TOP, monitors[m].rcMonitor.left + i * windowWidth, monitors[m].rcMonitor.top,
-                windowWidth, monitorHeight, SWP_NOZORDER);
-        }
-    }
-}
-
-void ApplyGridLayout(HWND* windows, int count) {
-    for (int m = 0; m < monitorCount; m++) {
-        int monitorHeight = monitors[m].rcMonitor.bottom - monitors[m].rcMonitor.top;
-        int monitorWidth = monitors[m].rcMonitor.right - monitors[m].rcMonitor.left;
-        int rows = (int)sqrt(count);
-        int cols = (count + rows - 1) / rows;  // Ceiling of count / rows
-
-        int windowWidth = monitorWidth / cols;
-        int windowHeight = monitorHeight / rows;
-
-        for (int i = 0; i < count; i++) {
-            int row = i / cols;
-            int col = i % cols;
-            SetWindowPos(windows[i], HWND_TOP, monitors[m].rcMonitor.left + col * windowWidth, monitors[m].rcMonitor.top + row * windowHeight,
-                windowWidth, windowHeight, SWP_NOZORDER);
-        }
-    }
+    return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -197,7 +80,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
             RECT rect;
             GetClientRect(hwnd, &rect); // Get the dimensions of the window's client area
-            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1)); // Fill the background
+            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW+1)); // Fill the background
 
             DrawText(hdc, L"Hello, World!", -1, &rect, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
 
@@ -209,7 +92,133 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             InvalidateRect(hwnd, NULL, TRUE);
             return 0;
         }
+        case WM_KEYDOWN: {
+            if (wParam == VK_MENU) {  // Alt key pressed
+                if (GetKeyState(0x56) & 0x8000) {  // Alt + V
+                    currentLayout = VERTICAL_TILING;
+                    ApplyLayout(currentLayout);
+                } else if (GetKeyState(0x48) & 0x8000) {  // Alt + H
+                    currentLayout = HORIZONTAL_TILING;
+                    ApplyLayout(currentLayout);
+                } else if (GetKeyState(0x47) & 0x8000) {  // Alt + G
+                    currentLayout = GRID_LAYOUT;
+                    ApplyLayout(currentLayout);
+                }
+            }
+            return 0;
+        }
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
+    static int count = 0;  // Keep track of how many windows we've handled
+    HWND* windows = (HWND*)lParam;
+
+    // Filter out windows we don't want to handle
+    if (!IsWindowVisible(hwnd) || GetWindowTextLength(hwnd) == 0) {
+        return TRUE;  // Continue enumerating
+    }
+
+    // Add the window to our list
+    windows[count++] = hwnd;
+
+    return TRUE;  // Continue enumerating
+}
+
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) {
+    if (monitorCount < 10) {
+        MYMONITORINFOEX mi;
+        mi.Monitor.cbSize = sizeof(MONITORINFOEX);
+        GetMonitorInfo(hMonitor, &mi.Monitor);
+        mi.rcMonitor = mi.Monitor.rcMonitor;
+
+        monitors[monitorCount++] = mi;
+
+        wprintf(L"Monitor %d: left=%d, top=%d, right=%d, bottom=%d\n", monitorCount,
+            mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right, mi.rcMonitor.bottom);
+    }
+    return TRUE;
+}
+
+void ApplyLayout(LayoutType layout) {
+    HWND windows[50];
+    int windowCount = 0;
+
+    // Enumerate all visible top-level windows
+    EnumWindows(EnumWindowsProc, (LPARAM)windows);
+
+    // Apply the specified layout to the windows
+    switch (layout) {
+        case VERTICAL_TILING:
+            ApplyVerticalTiling(windows, windowCount);
+            break;
+        case HORIZONTAL_TILING:
+            ApplyHorizontalTiling(windows, windowCount);
+            break;
+        case GRID_LAYOUT:
+            ApplyGridLayout(windows, windowCount);
+            break;
+    }
+}
+
+void ApplyVerticalTiling(HWND* windows, int count) {
+    int windowIndex = 0;
+    int windowsPerMonitor = (count + monitorCount - 1) / monitorCount; // Ceiling of count / monitorCount
+
+    for (int m = 0; m < monitorCount; m++) {
+        int monitorHeight = monitors[m].rcMonitor.bottom - monitors[m].rcMonitor.top;
+        int monitorWidth = monitors[m].rcMonitor.right - monitors[m].rcMonitor.left;
+        int windowHeight = monitorHeight / windowsPerMonitor;
+
+        for (int i = 0; i < windowsPerMonitor && windowIndex < count; i++, windowIndex++) {
+            SetWindowPos(windows[windowIndex], HWND_TOP, monitors[m].rcMonitor.left, monitors[m].rcMonitor.top + i * windowHeight,
+                monitorWidth, windowHeight, SWP_NOZORDER);
+            wprintf(L"Vertical Tiling: Monitor %d, Window %d: left=%d, top=%d, width=%d, height=%d\n", m + 1, windowIndex + 1,
+                monitors[m].rcMonitor.left, monitors[m].rcMonitor.top + i * windowHeight, monitorWidth, windowHeight);
+        }
+    }
+}
+
+void ApplyHorizontalTiling(HWND* windows, int count) {
+    int windowIndex = 0;
+    int windowsPerMonitor = (count + monitorCount - 1) / monitorCount; // Ceiling of count / monitorCount
+
+    for (int m = 0; m < monitorCount; m++) {
+        int monitorHeight = monitors[m].rcMonitor.bottom - monitors[m].rcMonitor.top;
+        int monitorWidth = monitors[m].rcMonitor.right - monitors[m].rcMonitor.left;
+        int windowWidth = monitorWidth / windowsPerMonitor;
+
+        for (int i = 0; i < windowsPerMonitor && windowIndex < count; i++, windowIndex++) {
+            SetWindowPos(windows[windowIndex], HWND_TOP, monitors[m].rcMonitor.left + i * windowWidth, monitors[m].rcMonitor.top,
+                windowWidth, monitorHeight, SWP_NOZORDER);
+            wprintf(L"Horizontal Tiling: Monitor %d, Window %d: left=%d, top=%d, width=%d, height=%d\n", m + 1, windowIndex + 1,
+                monitors[m].rcMonitor.left + i * windowWidth, monitors[m].rcMonitor.top, windowWidth, monitorHeight);
+        }
+    }
+}
+
+void ApplyGridLayout(HWND* windows, int count) {
+    int windowIndex = 0;
+    int windowsPerMonitor = (count + monitorCount - 1) / monitorCount; // Ceiling of count / monitorCount
+
+    for (int m = 0; m < monitorCount; m++) {
+        int monitorHeight = monitors[m].rcMonitor.bottom - monitors[m].rcMonitor.top;
+        int monitorWidth = monitors[m].rcMonitor.right - monitors[m].rcMonitor.left;
+        int rows = (int)sqrt(windowsPerMonitor);
+        int cols = (windowsPerMonitor + rows - 1) / rows;  // Ceiling of windowsPerMonitor / rows
+
+        int windowWidth = monitorWidth / cols;
+        int windowHeight = monitorHeight / rows;
+
+        for (int i = 0; i < windowsPerMonitor && windowIndex < count; i++, windowIndex++) {
+            int row = i / cols;
+            int col = i % cols;
+            SetWindowPos(windows[windowIndex], HWND_TOP, monitors[m].rcMonitor.left + col * windowWidth, monitors[m].rcMonitor.top + row * windowHeight,
+                windowWidth, windowHeight, SWP_NOZORDER);
+            wprintf(L"Grid Layout: Monitor %d, Window %d: left=%d, top=%d, width=%d, height=%d\n", m + 1, windowIndex + 1,
+                monitors[m].rcMonitor.left + col * windowWidth, monitors[m].rcMonitor.top + row * windowHeight, windowWidth, windowHeight);
+        }
     }
 }
